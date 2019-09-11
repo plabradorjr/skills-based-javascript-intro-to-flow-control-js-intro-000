8ee92b3914a1923f2ca71a96f814e6f8f827565d
Pancho Labrador Jr <plabradorjr@gmail.com>
2019-09-11 17:18:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,11 +3,21 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!";
+  } else {
+    return "You are not a teenage";
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <=19) {
+    return "You are a teenager!";
+  } else if (age < 13) {
+    return "You are a kid";
+  } else {
+      return "You are a grownup";
+    }
 }
 
 function ternaryTeenager(age) {
